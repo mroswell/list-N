55babdcca1267c9d4ab54ea019978a1680f89782
Marjorie Roswell <mroswell@gmail.com>
2021-03-20 21:18:07 -0400

    Account for null data in a Use Site record

--- transform.py
+++ transform.py
@@ -9,7 +9,8 @@ def transform(d):
         d["ACTI_ING"] = d["ACTI_ING"].split("; ")   # Active Ingredient
         d["FORM_TYPE"] = d["FORM_TYPE"].split("; ") # Formulation Type
         d["USE_SURF"] = d["USE_SURF"].split("; ")   # Surface Type
-        d["USE_SITE"] = d["USE_SITE"].split("; ")   # Use Site
+        if d["USE_SITE"] is not None:
+            d["USE_SITE"] = d["USE_SITE"].split("; ")   # Use Site
         yield d
 
 if __name__ == "__main__":
