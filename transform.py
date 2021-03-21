286c69dd7bf807b9b3bdef0c4e6facfd73508bc3
Marjorie Roswell <mroswell@gmail.com>
2021-03-20 20:45:48 -0400

    Update transform.py

--- transform.py
+++ transform.py
@@ -2,16 +2,16 @@ import json
 import sys
 
 def transform(d):
-  columns = d["COLUMNS"]
-	data = d["DATA"]
-	for row in data:
-    d = dict(zip(columns, row))
-    d["ACTI_ING"] = d["ACTI_ING"].split("; ")   # Active Ingredient
-    d["FORM_TYPE"] = d["FORM_TYPE"].split("; ") # Formulation Type
-    d["USE_SURF"] = d["USE_SURF"].split("; ")   # Surface Type
-    d["USE_SITE"] = d["USE_SITE"].split("; ")   # Use Site
-    yield d
+    columns = d["COLUMNS"]
+    data = d["DATA"]
+    for row in data:
+        d = dict(zip(columns, row))
+        d["ACTI_ING"] = d["ACTI_ING"].split("; ")   # Active Ingredient
+        d["FORM_TYPE"] = d["FORM_TYPE"].split("; ") # Formulation Type
+        d["USE_SURF"] = d["USE_SURF"].split("; ")   # Surface Type
+        d["USE_SITE"] = d["USE_SITE"].split("; ")   # Use Site
+        yield d
 
 if __name__ == "__main__":
-	data = json.load(sys.stdin)
-	print(json.dumps(list(transform(data))))
+    data = json.load(sys.stdin)
+    print(json.dumps(list(transform(data))))
